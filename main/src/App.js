11024ad76e883baf5e1ccf1e268eefd3ff930ac5
johnmartinroque <johnmartinroque@gmail.com>
2025-07-21 20:44:21 +0800

    header

--- main/src/App.js
+++ main/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Register from "./components/authentication/Register";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Header from "./others/Header";
+import Home from "./others/Home";
 function App() {
   return (
     <div>
-      <Register />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
