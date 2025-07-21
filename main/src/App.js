0145f3ccfcb0a6838092672b7569421bd19ab144
johnmartinroque <johnmartinroque@gmail.com>
2025-07-21 19:47:53 +0800

    firebase stuff

--- main/src/App.js
+++ main/src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
 function App() {
   return (
     <div className="App">
-      <Row>
-        <Col>Fam Fund</Col>
-      </Row>
+      <h1>App</h1>
     </div>
   );
 }
