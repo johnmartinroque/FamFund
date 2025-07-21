4756d46a0363ac4f54edd0d47f536fb532a19197
johnmartinroque <johnmartinroque@gmail.com>
2025-07-21 16:28:16 +0800

    first commit

---
+++ main/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Row, Col } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+      <Row>
+        <Col>Fam Fund</Col>
+      </Row>
+    </div>
+  );
+}
+
+export default App;
