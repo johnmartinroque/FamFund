b8d5a7763006dc8b3b2428ad1f800d85f5109cdf
johnmartinroque <johnmartinroque@gmail.com>
2025-07-21 20:32:13 +0800

    register

--- main/src/components/authentication/Register.jsx
+++ main/src/components/authentication/Register.jsx
@@ -1,7 +1,8 @@
-import { Button, Row, Col } from "react-bootstrap";
+import { Button, Row, Col, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { auth, db } from "../../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 function Register() {
   const [email, setEmail] = useState("");
@@ -9,55 +10,63 @@ function Register() {
   const auth = getAuth();
   const register = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log(errorCode, "+", errorMessage);
-        });
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        email: user.email,
+      });
+
       setEmail("");
       setPassword("");
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.error("Registration Error: ", error.code, error.message);
     }
   };
+
+  useEffect(() => {});
   return (
     <div>
-      <Row>
-        <Col>Register</Col>
-        <Col>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">
-              Example textarea
-            </label>
-            <textarea
-              class="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></textarea>
-          </div>
-          <Button onClick={register}>Submit</Button>
-        </Col>
-      </Row>
+      <Container>
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <h1 className="text-center">Register</h1>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">
+                Password
+              </label>
+              <input
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <div className="text-center">
+              <Button onClick={register}>Submit</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
